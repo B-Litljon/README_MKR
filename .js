8874117297ab6e745c30ace51d314b7ef25229e5
Branidni <98710658+Branidni@users.noreply.github.com>
2022-11-15 12:00:15 -0800

    initial setup

---
+++ .js
@@ -0,0 +1,18 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+ //---------------
+//-inquirer inquiries-
+
+
+ //----------------
+//-todo goes here-
+
+ //----------------
+//-todo goes here-
+
+ //----------------
+//-todo goes here-
+
+ //----------------
+//-todo goes here-
