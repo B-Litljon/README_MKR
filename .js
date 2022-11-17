50095c97b362395d5f909977953e1df81a7992ae
Branidni <98710658+Branidni@users.noreply.github.com>
2022-11-16 16:46:20 -0800

    add test file

--- .js
+++ .js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+//const { title } = require('process');
+//const { data } = require('cheerio/lib/api/attributes');
 
  //---------------
 //-inquirer inquiries-
@@ -20,10 +22,10 @@ inquirer.prompt(   //prompt method arguments should be wrapped in this order (re
         },
         
         { //table of contents
-            type: 'confirm',
+            type: 'input',
             message: 'would you like to include a table of contents?',
-            name: 'T.O.C',
-            validate: (value)=>{if (value){return true} else {return "its either yes or no this ones easy -_-"} }  // include a function here later after you've achieved mvp, this function will take user input for the TOC
+            name: 'contentsTable',
+            validate: (value)=>{if (value){return true} else {return "type somethin will ya -_-"} }  // include a function here later after you've achieved mvp, this function will take user input for the TOC
         },
 
         { //how to install
@@ -37,7 +39,7 @@ inquirer.prompt(   //prompt method arguments should be wrapped in this order (re
             type: 'input',
             message: 'how do we use this app?',
             name: 'usage',
-            validate: (value)=>{if (value){return true} else {return " "} }  
+            validate: (value)=>{if (value){return true} else {return "type somethin will ya -_- "} }  
         },
         
         { //license
@@ -45,40 +47,71 @@ inquirer.prompt(   //prompt method arguments should be wrapped in this order (re
             message: "is there any specific license you'd like to use?",
             name: 'license',
             choices: ['MIT', 'ISC', 'GPL', 'N/A'],
-            validate: (value)=>{if (value){return true} else {return "if you don't care all you gatta put is NA"} }  
+            validate: (value)=>{if (value){return true} else {return "if you don't care all you gotta do is click N/A"} }  
         },
         
         { //how to contribute
             type: 'input',  //change to 'list' later after mvp is achieved
-            message: "if you'd like to help I'd greatly appreciate, as i am poor and live with my parents :')",
+            message: "if you'd like to contribute I'd greatly appreciate, as i am poor and live with my parents :')",
             name: 'contributions',
             validate: (value)=>{if (value){return true} else {return "c'mon, Lemme hold a dollah... pls :'''( "} }  
         },
         
         { //tests
-            type: 'confirm',
+            type: 'input',
             message: 'include tests?',
             name: 'tests',
-            validate: (value)=>{if (value){return true} else {return "I've run out of clever things to say just click yes or no!"} }  
+            validate: (value)=>{if (value){return true} else {return "type somethin will ya -_-"} }  
         },
         
-        { //questions
+        { //contact
             type: 'input',
-            message: "if you've got any questions don't hesitate to reach out to me on github :)",
-            name: 'questions',
+            message: "how can people contact you?",
+            name: 'contact',
             validate: (value)=>{if (value){return true} else {return "you're almost done! this is the last one just put anything!"} }  
         },
     ]
-)
+).then(({
+    title,
+    description,
+    contentsTable,
+    install,
+    usage,
+    licsense,
+    contributions,
+    tests,
+    contact
 
- //----------------
-//-file writer-
-
- //----------------
-//-todo goes here-
-
- //----------------
-//-todo goes here-
-
- //----------------
-//-todo goes here-
+})=>{ //markdown template
+const template = `# ${title}   
+* [description] (#description)
+* [contentsTable] (#contentsTable)  
+* [install] (#install)  //
+* [usage] (#usage) //alter
+* [licsense] (#licsense)
+* [contributions] (#contributions)
+* [tests] (#tests)  //
+* [contact] (#contact)
+# description
+${description}
+# install
+${install}
+# usage
+${usage}
+# license
+${licsense}
+# contributions
+${contributions}
+# contact
+${contact}`;  
+//function to create the file
+makeNewFile(title, template);
+ }
+);
+function makeNewFile(fileName, data){
+fs.writeFile(`./${fileName.toLowerCase().split(' ').join(' ')}.md`,data,()=>{
+        //if(err) ()=> {console.log(err)}
+        console.log("you're now the proud parent of a professional README :) ");
+    })
+}
+ 
---
+++ demo.md
@@ -0,0 +1,21 @@
+# demo   
+* [description] (#description)
+* [contentsTable] (#contentsTable)  
+* [install] (#install)  //
+* [usage] (#usage) //alter
+* [licsense] (#licsense)
+* [contributions] (#contributions)
+* [tests] (#tests)  //
+* [contact] (#contact)
+# description
+just testing things
+# install
+idk yet
+# usage
+don't
+# license
+undefined
+# contributions
+no thanks
+# contact
+don't do that
